450160c529528999788cea42ee322cc7ea86b62c
Tanayyyyy <manntanayverma@gmail.com>
2022-04-04 00:44:52 +0530

    Register page starting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Topbar/>
+      <Register />
+      {/* <Topbar/> */}
     </div>
   );
 }
