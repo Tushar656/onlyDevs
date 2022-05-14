f2befcc92f44fe24ff680e4b83cd64e4e85cf3fb
Tushar <tushar55755575@gmail.com>
2022-05-14 15:46:18 +0530

    make Modals for Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/OLY')
+.then(console.log("Connection successfull"))
+.catch((err) => console.log("Not connected to mongodb"));
+
 app.get('/', function (req, res) {
     res.send('Hello World');
  })
